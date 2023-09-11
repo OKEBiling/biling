0d97ad4157f6e53af5d8c6eda167140d6208ae61
OKEBILING <ridwan@okenet.net.id>
2023-09-11 10:48:06 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ini adalah biling
+Aturan pembuatan class untuk menangani sebuah page.
+1.Nama file harus dengan format (namafile)Controller.php diawali dengan huruf kecil misalnya loginController.php 
+2.Nama class harus sama dengan nama file misalnya class loginController nama file juga harus loginController.php
+Layout berada di folder views nama file bebas karena nanti kita yang akan menuliskanya di program misalnya login.php
+lalu rendering misalkan file login.php dengan cara 
+1.  `$this->renderView('namafile', $data = []);';` jadi ` $this->renderView('login',  = [])`;
+2.   
+
+
+    
+ 
--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,1 +1,5 @@
-!function(){"use strict";"dark-mode-switch"==sessionStorage.getItem("is_visited")?document.documentElement.setAttribute("data-bs-theme","dark"):"light-mode-switch"!=sessionStorage.getItem("is_visited")&&"rtl-mode-switch"!=sessionStorage.getItem("is_visited")||document.documentElement.setAttribute("data-bs-theme","light")}();
+!
+function() {
+    "use strict";
+    "dark-mode-switch" == sessionStorage.getItem("is_visited") ? document.documentElement.setAttribute("data-bs-theme", "dark") : "light-mode-switch" != sessionStorage.getItem("is_visited") && "rtl-mode-switch" != sessionStorage.getItem("is_visited") || document.documentElement.setAttribute("data-bs-theme", "light")
+} ();
