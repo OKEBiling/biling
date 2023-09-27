8e2fa61d7c22a428332db0649532de19c600bba9
OKEBILING <ridwan@okenet.net.id>
2023-09-27 10:11:07 +0700

    update maps issue

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -10,12 +10,14 @@ var customerHandle = (function() {
     _createClass(customerHandle, [{
         key: "init",
         value: function init() {
+            this.ValidasiForm();
+            this.SelectWil();
+         if (document.getElementById("map")) {
             this.showmaps();
             this.defaultmarker();
             this.leafetevent();
             this.customkoordinat();
-            this.ValidasiForm();
-            this.SelectWil();
+        }
         }
     }, {
         key: "showmaps",
@@ -25,13 +27,16 @@ var customerHandle = (function() {
                 transform3DLimit: false,
                 zoomControl: false,
             }).setView(this.defaultOrigin, this.defaultZoom);
+            
             L.tileLayer(`https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}`, {
                 maxZoom: 22,
                 minZoom: 2,
                 attribution: false,
                 subdomains: ["mt0", "mt1", "mt2", "mt3"],
             }).addTo(this.myLeafletMaps);
-        }
+            
+
+       }
     }, {
         key: 'defaultmarker',
         value: function defaultmarker() {
@@ -39,7 +44,6 @@ var customerHandle = (function() {
                 draggable: true
             }).addTo(this.myLeafletMaps);
         }
-
     }, {
         key: "leafetevent",
         value: function leafetevent() {
@@ -83,96 +87,69 @@ var customerHandle = (function() {
     }, {
         key: "ValidasiForm",
         value: function ValidasiForm() {
-const addCustomer = document.querySelector("#addCustomer");
-
-
-    var t;
-    addCustomer && FormValidation.formValidation(addCustomer, {
-        fields: {
-            firstname: {
-                validators: {
-                    notEmpty: {
-                        message: "Please enter firstname"
+        const addCustomer = document.querySelector("#addCustomer");
+        
+        
+            var t,y;
+            addCustomer && FormValidation.formValidation(addCustomer, {
+                fields: {
+                    firstname: {
+                        validators: {
+                            notEmpty: {
+                                message: "Please enter firstname"
+                            },
+                        }
                     },
-                }
-            },
-            lastname: {
-                validators: {
-                    notEmpty: {
-                        message: "Please enter your lastname"
+                    lastname: {
+                        validators: {
+                            notEmpty: {
+                                message: "Please enter  lastname"
+                            },
+                        }
                     },
-                }
-            },
-            phone: {
-                validators: {
-                    notEmpty: {
-                        message: "Please enter your password"
+                    phoneNumber: {
+                        validators: {
+                            notEmpty: {
+                                message: "Please enter  phone Number"
+                            },
+                        }
                     },
-                    stringLength: {
-                        min: 6,
-                        message: "Password must be more than 6 characters"
-                    }
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger,
+                    bootstrap5: new FormValidation.plugins.Bootstrap5({
+                        eleValidClass: "",
+                        rowSelector: ".col-md-6"
+                    }),
+                    submitButton: new FormValidation.plugins.SubmitButton,
+                    defaultSubmit: new FormValidation.plugins.DefaultSubmit,
+                    autoFocus: new FormValidation.plugins.AutoFocus
+                },
+                init: e => {
+                    e.on("plugins.message.placed", function(e) {
+                        e.element.parentElement.classList.contains("input-group") && e.element.parentElement.insertAdjacentElement("afterend", e.messageElement)
+                    })
                 }
-            },
-            "confirm-password": {
-                validators: {
-                    notEmpty: {
-                        message: "Please confirm password"
-                    },
-                    identical: {
-                        compare: function() {
-                            return addCustomer.querySelector('[name="password"]').value
-                        },
-                        message: "The password and its confirm are not the same"
-                    },
-                    stringLength: {
-                        min: 6,
-                        message: "Password must be more than 6 characters"
-                    }
-                }
-            },
-            terms: {
-                validators: {
-                    notEmpty: {
-                        message: "Please agree terms & conditions"
-                    }
-                }
-            }
-        },
-        plugins: {
-            trigger: new FormValidation.plugins.Trigger,
-            bootstrap5: new FormValidation.plugins.Bootstrap5({
-                eleValidClass: "",
-                rowSelector: ".col-md-6"
-            }),
-            submitButton: new FormValidation.plugins.SubmitButton,
-            defaultSubmit: new FormValidation.plugins.DefaultSubmit,
-            autoFocus: new FormValidation.plugins.AutoFocus
-        },
-        init: e => {
-            e.on("plugins.message.placed", function(e) {
-                e.element.parentElement.classList.contains("input-group") && e.element.parentElement.insertAdjacentElement("afterend", e.messageElement)
+            }), (t = document.querySelectorAll(".numeral-mask")).length && t.forEach(e => {
+                new Cleave(e, {
+                    numeral: !0
+                })
+            }),(y = document.querySelectorAll(".phone-mask")).length && y.forEach(e => {
+                   var cleave = new Cleave(e, {
+                    numericOnly : true,
+                    blocks: [4,4,4,3],
+                    delimiters: [' ',' ', ' ', ' '],
+                    prefix: '+62'
+                });
             })
-        }
-    }), (t = document.querySelectorAll(".numeral-mask")).length && t.forEach(e => {
-        new Cleave(e, {
-            numeral: !0
-        })
-    })
-
 
         }
     }, {
         key: "SelectWil",
         value: function SelectWil() {
-
             function clearOptions(id) {
-            console.log("on clearOptions :" + id);
-
-            //$('#' + id).val(null);
             $('#' + id).empty().trigger('change');
         }
-      // Fungsi untuk mengisi pilihan dalam elemen select
       function populateSelect(element, data, placeholder) {
         element.empty();
         element.select2({
@@ -181,7 +158,6 @@ const addCustomer = document.querySelector("#addCustomer");
         });
       }
 
-      // Fungsi untuk mengambil data dari file JSON
       function loadData(element, url, placeholder) {
         $.getJSON(url, function(data) {
           data = [{
@@ -196,67 +172,38 @@ const addCustomer = document.querySelector("#addCustomer");
         });
       }
 
-      // Inisialisasi Select2 untuk Provinsi
       var $selectProvinsi = $('#Provinsi');
       loadData($selectProvinsi, '/assets/data/provinsi.json', '- Pilih Provinsi -');
-
-      // Handler untuk saat Provinsi dipilih
       $selectProvinsi.on('change', function() {
         var selectedProvinsiId = $(this).val();
         clearOptions('Kabupaten');
 
         if (selectedProvinsiId) {
-          console.log("on change selectProv");
-
           var text = $selectProvinsi.find(':selected').text();
-          console.log("value = " + selectedProvinsiId + " / " + "text = " + text);
-
-          console.log('Load Kabupaten di ' + text + '...')
           loadData($('#Kabupaten'), '/assets/data/kabupaten/' + selectedProvinsiId + '.json', '- Pilih Kabupaten -');
         }
       });
-
-      // Handler untuk saat Kabupaten dipilih
       $('#Kabupaten').on('change', function() {
         var selectedKabupatenId = $(this).val();
         clearOptions('Kecamatan');
-
         if (selectedKabupatenId) {
-          console.log("on change selectKab");
-
           var text = $('#Kabupaten :selected').text();
-          console.log("value = " + selectedKabupatenId + " / " + "text = " + text);
-
-          console.log('Load Kecamatan di ' + text + '...')
           loadData($('#Kecamatan'), '/assets/data/kecamatan/' + selectedKabupatenId + '.json', '- Pilih Kecamatan -');
         }
       });
-
-      // Handler untuk saat Kecamatan dipilih
       $('#Kecamatan').on('change', function() {
         var selectedKecamatanId = $(this).val();
         clearOptions('Kelurahan');
-
         if (selectedKecamatanId) {
-          console.log("on change selectKec");
-
           var text = $('#Kecamatan :selected').text();
-          console.log("value = " + selectedKecamatanId + " / " + "text = " + text);
-
-          console.log('Load Kelurahan di ' + text + '...')
           loadData($('#Kelurahan'), '/assets/data/kelurahan/' + selectedKecamatanId + '.json', '- Pilih Kelurahan -');
         }
       });
-
-      // Handler untuk saat Kelurahan dipilih
       $('#Kelurahan').on('change', function() {
         var selectedKelurahanId = $(this).val();
 
         if (selectedKelurahanId) {
-          console.log("on change selectKel");
-
           var text = $('#Kelurahan :selected').text();
-          console.log("value = " + selectedKelurahanId + " / " + "text = " + text);
         }
       });
     $(document).on('select2:open', () => {
